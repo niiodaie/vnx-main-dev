3c3bc68aea2b657033509d4e084da3eaf65fafb6
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-06-08 02:17:09 -0500

    Add files via upload

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
-export default function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 text-center p-10">
-      <h1 className="text-4xl font-bold text-green-600">Tailwind is  LIVE on Vercel 🚀</h1>
-    </div>
-  );
-}
+import React from 'react';
+
+const App = () => {
+  return <h1 className="text-3xl font-bold text-green-600">Tailwind is LIVE on Vercel 🚀</h1>;
+};
+
+export default App;
