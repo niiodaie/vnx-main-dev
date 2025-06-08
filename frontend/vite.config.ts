ce39a39711d8bbd3bde8a4360678a3f07e1bb8e5
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-06-07 22:15:34 -0500

    Add files via upload

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,19 +2,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    minify: 'esbuild',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  },
   css: {
     postcss: './postcss.config.js'
   }
