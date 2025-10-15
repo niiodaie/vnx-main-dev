9b279e46a5f27c1cc268691bab5b435181ce95d6
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-10-15 17:01:19 -0500

    Refactor PostCSS config to use object syntax

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,10 @@ import autoprefixer from "autoprefixer";
 
 /** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: [tailwindcss, autoprefixer],
+  plugins: {
+    tailwindcss,
+    autoprefixer,
+  },
 };
 
 export default config;
