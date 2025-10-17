89eb18efc8f0eb9553457a1dc38fe97c4f48c067
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-10-17 15:47:30 -0500

    Update index.ts

--- lib/payments/index.ts
+++ lib/payments/index.ts
@@ -1,10 +1,21 @@
+// lib/payments/index.ts
+
 import { createLemonCheckout } from "./lemon";
 import { createStripeCheckout } from "./stripe";
 
-export async function createCheckoutSession(email: string) {
+/**
+ * Hybrid checkout selector
+ * Automatically switches between Stripe and LemonSqueezy based on ENV config
+ */
+export async function createCheckoutSession(email: string, priceId: string) {
   const useStripe = process.env.NEXT_PUBLIC_USE_STRIPE === "true";
 
-  return useStripe
-    ? await createStripeCheckout(email)
-    : await createLemonCheckout(email);
+  if (useStripe) {
+    return await createStripeCheckout(email, priceId);
+  } else {
+    return await createLemonCheckout(email, priceId);
+  }
 }
+
+export * from "./stripe";
+export * from "./stripeClient";
