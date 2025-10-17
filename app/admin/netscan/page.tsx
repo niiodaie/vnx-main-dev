a59af3c24b50ee4cce579385b9cc438ca13e7176
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-10-17 18:03:36 -0500

    Update page.tsx

--- app/admin/netscan/page.tsx
+++ app/admin/netscan/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 export const dynamic = "force-dynamic";
+export const fetchCache = 'force-no-store';
 import { useEffect, useState } from 'react';
 import { useAuth } from '@/lib/auth/AuthContext';
 import { useRouter } from 'next/navigation';
