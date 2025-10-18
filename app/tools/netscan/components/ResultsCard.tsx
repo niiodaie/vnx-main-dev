2c04d591f4a6a273f994f6461d27633e5b8e779e
niiodaie <102459031+niiodaie@users.noreply.github.com>
2025-10-18 16:05:10 -0500

    Update ResultsCard.tsx

--- app/tools/netscan/components/ResultsCard.tsx
+++ app/tools/netscan/components/ResultsCard.tsx
@@ -1,4 +1,6 @@
-import { Card, CardContent } from '../../../components/ui/card'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent } from '@/components/ui/card'
+
 
 export default function ResultsCard({ data }: { data: any }) {
   return (
